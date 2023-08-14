a20a20128bc513405318f838e75c5c03feac3fa3
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-08-15 00:53:27 +0200

    Added overlay to close the menu

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -30,26 +30,22 @@ const Header = () => {
                 <Popover.Button className="flex items-center">
                   <Bars3Icon className="h-10 w-10" />
                 </Popover.Button>
-                {open && (
-                  <>
-                    <div className="fixed left-0 top-0 h-screen w-full bg-red-800/60 bg-white"></div>
-                    <Popover.Panel
-                      className="fixed bottom-0 left-0 top-0 h-full w-60 bg-white dark:bg-gray-700"
-                      static
-                    >
-                      <div className="flex h-16 items-center gap-4 border-b-2 px-7 dark:border-gray-600">
-                        <button onClick={close}>
-                          <Bars3Icon className="h-10 w-10" />
-                        </button>
-                        <Link to="/" className="text-2xl" onClick={close}>
-                          Sewaddle
-                        </Link>
-                      </div>
+                <Popover.Panel className="fixed bottom-0 left-0 top-0 z-50 h-full w-60 bg-white dark:bg-gray-700">
+                  <div className="flex h-16 items-center gap-4 border-b-2 px-7 dark:border-gray-600">
+                    <button onClick={close}>
+                      <Bars3Icon className="h-10 w-10" />
+                    </button>
+                    <Link to="/" className="text-2xl" onClick={close}>
+                      Sewaddle
+                    </Link>
+                  </div>
 
-                      <BigSidebarButtons />
-                    </Popover.Panel>
-                  </>
-                )}
+                  <BigSidebarButtons />
+                </Popover.Panel>
+                <Popover.Overlay
+                  className="fixed bottom-0 left-0 right-0 top-0 cursor-pointer"
+                  as="div"
+                ></Popover.Overlay>
               </>
             )}
           </Popover>
