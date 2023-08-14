d7f8b1dc8eeef56f2671664a7268146c5e4a7a4e
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-08-14 21:51:13 +0200

    Added ability to goto the next page

--- src/pages/ViewPage.tsx
+++ src/pages/ViewPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 import { useChapter, useNextChapter } from "../api/manga";
@@ -9,6 +10,8 @@ const ViewPage = () => {
   const chapterQuery = useChapter({ id });
   const nextChapterQuery = useNextChapter({ id });
 
+  const [currentPage, setCurrentPage] = useState(0);
+
   if (chapterQuery.isError) return <p className="dark:text-white">Error</p>;
   if (chapterQuery.isLoading)
     return <p className="dark:text-white">Loading...</p>;
@@ -16,22 +19,43 @@ const ViewPage = () => {
   const { data } = chapterQuery;
   const { data: nextChapter } = nextChapterQuery;
 
-  return (
-    <div className="flex flex-col gap-2 items-center">
-      {data.pages.map((page, i) => {
-        return (
-          <img
-            className={`w-auto h-auto max-h-[100vh]`}
-            src={pb.getFileUrl(data, page)}
-            alt=""
-            key={i}
-          />
-        );
-      })}
+  const nextPage = () => {
+    // TODO(patrik): Check for overflow
+    setCurrentPage(currentPage + 1);
+  };
 
-      {nextChapter && (
-        <Link to={`/view/${nextChapter.next}`}>Next Chapter</Link>
-      )}
+  const prevPage = () => {
+    // TODO(patrik): Check for underflow
+    setCurrentPage(currentPage - 1);
+  };
+
+  const getCurrentPageUrl = () => {
+    return pb.getFileUrl(data, data.pages[currentPage]);
+  };
+
+  return (
+    <div className="relative flex h-full w-full justify-center">
+      <button
+        className="absolute bg-red-400/20 h-full w-1/2 left-0"
+        onClick={nextPage}
+      ></button>
+
+      <img
+        className={"h-full object-scale-down"}
+        src={getCurrentPageUrl()}
+        alt=""
+      />
+
+      <button
+        className="absolute bg-blue-400/20 h-full w-1/2 right-0"
+        onClick={prevPage}
+      ></button>
+
+      {/* {nextChapter && (
+        <Link className="dark:text-white" to={`/view/${nextChapter.next}`}>
+          Next Chapter
+        </Link>
+      )} */}
     </div>
   );
 };
