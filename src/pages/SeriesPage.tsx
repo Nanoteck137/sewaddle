6866316a6d2a84e62f78d04ee5c92dac6ae71c64
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-08-15 22:46:45 +0200

    Testing stuff with the series page

--- src/pages/SeriesPage.tsx
+++ src/pages/SeriesPage.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 import { useManga, useMangaChaptersBasic } from "../api/manga";
@@ -12,7 +12,7 @@ const Chapter = (props: { chapter: BasicChapter }) => {
   return (
     <Link
       to={`/view/${chapter.id}`}
-      className="flex flex-col items-center gap-2 rounded bg-gray-600 p-2"
+      className="flex flex-col items-center gap-2 rounded border-2 p-2 shadow dark:border-gray-500 dark:bg-gray-600"
     >
       <img
         className="h-44"
@@ -32,6 +32,8 @@ const SeriesPage = () => {
   const mangaQuery = useManga({ id });
   const mangaChaptersQuery = useMangaChaptersBasic({ id });
 
+  const [collapsed, setCollapsed] = useState(true);
+
   if (mangaQuery.isError || mangaChaptersQuery.isError) return <p>Error</p>;
   if (mangaQuery.isLoading || mangaChaptersQuery.isLoading)
     return <p>Loading...</p>;
@@ -41,46 +43,76 @@ const SeriesPage = () => {
 
   return (
     <div className="flex flex-col gap-10 p-2">
-      <div>
-        <p>Name: {manga.name}</p>
-        <p className="whitespace-pre-wrap">{manga.description}</p>
-        <a href={manga.malUrl} target="_blank">
-          MAL Link
-        </a>
-        <p>Chapters Available: {manga.chaptersAvailable}</p>
-        <img
-          className="w-64"
-          src={
-            isValidHttpUrl(manga.cover)
-              ? manga.cover
-              : pb.getFileUrl(manga, manga.cover)
-          }
-          alt=""
-        />
+      <p className="text-center text-2xl">{manga.name}</p>
+      <div className="grid grid-cols-1 place-items-center md:grid-cols-3">
+        <div className="flex h-full flex-col justify-start">
+          <img
+            className=""
+            src={
+              isValidHttpUrl(manga.cover)
+                ? manga.cover
+                : pb.getFileUrl(manga, manga.cover)
+            }
+            alt=""
+          />
+
+          <a href={manga.malUrl} target="_blank">
+            MyAnimeList
+          </a>
+        </div>
+
+        {/* <div className="col-span-2 flex h-full flex-col justify-between">
+          <div>
+            <p
+              className={`w-full md:hidden ${
+                collapsed
+                  ? "line-clamp-1 overflow-hidden"
+                  : "whitespace-pre-wrap"
+              }`}
+            >
+              {manga.description}
+            </p>
+            <button
+              className="rounded bg-red-400 px-6 py-2 md:hidden"
+              onClick={() => {
+                setCollapsed(!collapsed);
+              }}
+            >
+              Toggle
+            </button>
+            <div className="hidden md:block">
+              <p className="whitespace-pre-wrap">{manga.description}</p>
+            </div>
+          </div> */}
+        {/* </div> */}
       </div>
-      <div className="grid grid-cols-2 gap-2 md:grid-cols-4 lg:grid-cols-5">
-        {chapters.pages.map((page, i) => {
-          return (
-            <Fragment key={i}>
-              {page.items.map((item) => {
-                return <Chapter key={item.id} chapter={item} />;
-              })}
-            </Fragment>
-          );
-        })}
-        <button
-          onClick={() => mangaChaptersQuery.fetchNextPage()}
-          disabled={
-            !mangaChaptersQuery.hasNextPage ||
-            mangaChaptersQuery.isFetchingNextPage
-          }
-        >
-          {mangaChaptersQuery.isFetchingNextPage
-            ? "Loading more..."
-            : mangaChaptersQuery.hasNextPage
-            ? "Load More"
-            : "Nothing more to load"}
-        </button>
+
+      <div className="flex flex-col gap-2">
+        <p>Chapters Available: {manga.chaptersAvailable}</p>
+        <div className="grid grid-cols-2 gap-2 md:grid-cols-4 lg:grid-cols-5">
+          {chapters.pages.map((page, i) => {
+            return (
+              <Fragment key={i}>
+                {page.items.map((item) => {
+                  return <Chapter key={item.id} chapter={item} />;
+                })}
+              </Fragment>
+            );
+          })}
+          <button
+            onClick={() => mangaChaptersQuery.fetchNextPage()}
+            disabled={
+              !mangaChaptersQuery.hasNextPage ||
+              mangaChaptersQuery.isFetchingNextPage
+            }
+          >
+            {mangaChaptersQuery.isFetchingNextPage
+              ? "Loading more..."
+              : mangaChaptersQuery.hasNextPage
+              ? "Load More"
+              : "Nothing more to load"}
+          </button>
+        </div>
       </div>
     </div>
   );
