d51d87197ac631a6a7e39d3a2f269902004393ce
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-08-20 18:24:52 +0200

    Working on displaying user saved mangas

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,42 +1,5 @@
-import { Link } from "react-router-dom";
-
 import { useMangas } from "../api/manga";
-import { pb } from "../api/pocketbase";
-import { MangaView } from "../models/manga";
-
-const Item = (props: { manga: MangaView }) => {
-  const { manga } = props;
-  return (
-    <Link
-      to={`/series/${manga.id}`}
-      className={`relative flex h-full max-h-[500px] max-w-xs flex-col items-center overflow-hidden rounded  border-2 bg-white shadow-md dark:border-gray-500 dark:bg-gray-600 md:max-w-sm`}
-    >
-      <div className="absolute right-2 top-2 flex h-12 w-12 items-center justify-center rounded-full bg-blue-600/90 text-white">
-        <p>{manga.chaptersAvailable}</p>
-      </div>
-      <img
-        className="h-full w-full overflow-clip object-cover"
-        src={pb.getFileUrl(manga, manga.coverExtraLarge)}
-        alt=""
-      />
-      <p className="p-4 text-center">{manga.englishTitle}</p>
-    </Link>
-  );
-};
-
-const MangaList = (props: { list: MangaView[] }) => {
-  const { list } = props;
-
-  return (
-    <>
-      <div className="grid grid-cols-1 place-items-center gap-4 p-2 md:grid-cols-2 lg:grid-cols-4">
-        {list.map((item) => {
-          return <Item key={item.id} manga={item} />;
-        })}
-      </div>
-    </>
-  );
-};
+import MangaList from "../components/MangaList";
 
 const HomePage = () => {
   const { data, isLoading, isError } = useMangas();
--- src/pages/SavedPage.tsx
+++ src/pages/SavedPage.tsx
@@ -1,5 +1,49 @@
+import { useQuery } from "@tanstack/react-query";
+import { Navigate } from "react-router-dom";
+import { z } from "zod";
+
+import { pb } from "../api/pocketbase";
+import MangaList from "../components/MangaList";
+import { useAuth } from "../contexts/AuthContext";
+import { Collection } from "../models/collection";
+import { MangaView } from "../models/manga";
+
+const Schema = z.array(
+  Collection.extend({
+    user: z.string(),
+    manga: z.string(),
+    expand: z.object({
+      manga: MangaView,
+    }),
+  }),
+);
+
 const SavedPage = () => {
-  return <p>Saved Page</p>;
+  const auth = useAuth();
+  const savedMangas = useQuery({
+    queryKey: ["savedManga", auth.user?.id],
+    queryFn: async () => {
+      const list = await pb
+        .collection("userMangaSaved")
+        .getFullList({ expand: "manga" });
+      return await Schema.parseAsync(list);
+    },
+    select: (data) => data.map((i) => i.expand.manga),
+    enabled: !!auth.user,
+  });
+
+  console.log(savedMangas.data);
+
+  if (savedMangas.isError) return <p>Error</p>;
+  if (savedMangas.isLoading) return <p>Loading...</p>;
+
+  // if (!auth.isLoggedIn) return <Navigate to="/" />;
+
+  return (
+    <div>
+      <MangaList list={savedMangas.data} />
+    </div>
+  );
 };
 
 export default SavedPage;
