3fa191f7be4de4f27e83f01910ac93a6d2bab1db
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-08-20 20:32:59 +0200

    Ability to select and mark chapters read/unread

--- src/pages/SeriesPage.tsx
+++ src/pages/SeriesPage.tsx
@@ -1,3 +1,9 @@
+import {
+  BookmarkIcon,
+  BookmarkSlashIcon,
+  BookOpenIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/solid";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import parse from "html-react-parser";
 import { forwardRef, Fragment, useEffect, useState } from "react";
@@ -18,19 +24,37 @@ const Chapter = forwardRef<
     isContinue: boolean;
     hasRead: boolean;
     isGroup: boolean;
+
+    select: (select: boolean) => void;
+    showSelectMarker: boolean;
+    disableSelectMarker: boolean;
+    isSelected: boolean;
   }
 >((props, ref) => {
-  const { chapter, isContinue, hasRead, isGroup } = props;
+  const {
+    chapter,
+    isContinue,
+    hasRead,
+    isGroup,
+    isSelected,
+    showSelectMarker,
+    disableSelectMarker,
+    select,
+  } = props;
 
   return (
     <Link
       ref={ref}
       to={`/view/${chapter.id}`}
-      className="relative flex flex-col items-center rounded border-2 shadow dark:border-gray-500 dark:bg-gray-600"
+      className="group relative flex flex-col items-center rounded border-2 shadow dark:border-gray-500 dark:bg-gray-600"
+      draggable="false"
+      onClick={(e) => {
+        if (showSelectMarker) {
+          e.preventDefault();
+          select(!isSelected);
+        }
+      }}
     >
-      {/* <div className="absolute left-4 top-4 flex w-10 items-center justify-center rounded-full border-2 border-black bg-red-400/95 text-black">
-        <p className="aspect-square">{chapter.idx}</p>
-      </div> */}
       {isGroup && (
         <p className="h-auto flex-grow p-1">
           Ch. {chapter.group} - {chapter.name}
@@ -42,11 +66,28 @@ const Chapter = forwardRef<
         <img
           className=""
           src={pb.getFileUrl(chapter, chapter.cover)}
+          draggable="false"
           alt="Chapter Cover"
         />
 
+        {!disableSelectMarker && (
+          <button
+            className={`absolute left-2 top-2 h-8 w-8 rounded border-2 border-gray-400  bg-black/80 ${
+              showSelectMarker ? "" : "hidden group-hover:block"
+            }`}
+            onClick={(e) => {
+              console.log("HELLO?");
+              e.stopPropagation();
+              e.preventDefault();
+              select(!isSelected);
+            }}
+          >
+            {isSelected && <XMarkIcon />}
+          </button>
+        )}
+
         <div
-          className={`absolute right-4 top-4 flex h-10 w-10 items-center justify-center rounded-full p-1 shadow-xl ${
+          className={`absolute right-2 top-2 flex h-10 w-10 items-center justify-center rounded-full p-1 shadow-xl ${
             isContinue
               ? "bg-yellow-400/95"
               : hasRead
@@ -169,6 +210,49 @@ const SeriesPage = () => {
   ]);
 
   const [collapsed, setCollapsed] = useState(true);
+  const [selectedItems, setSelectedItems] = useState<string[]>([]);
+
+  const allChapterIds = useQuery({
+    queryKey: ["allChapterIds", id],
+    queryFn: async () => {
+      const recs = await pb
+        .collection("chapters")
+        .getFullList({ filter: `manga = "${id}"` });
+      return await z.array(Collection.pick({ id: true })).parseAsync(recs);
+    },
+    select: (data) => data.map((i) => i.id),
+    enabled: !!id,
+  });
+
+  const markItems = useMutation({
+    mutationFn: async (input: { items: string[]; markAsRead: boolean }) => {
+      if (auth.user) {
+        if (input.markAsRead) {
+          const promises = input.items.map((id) => {
+            return pb.collection("userChapterRead").create(
+              {
+                user: auth.user.id,
+                chapter: id,
+              },
+              { $autoCancel: false },
+            );
+          });
+
+          await Promise.all(promises);
+        }
+      }
+    },
+
+    onSuccess: () => {
+      setSelectedItems([]);
+    },
+
+    onSettled: () => {
+      if (auth.user) {
+        queryClient.invalidateQueries(["chapterRead", auth.user.id, id]);
+      }
+    },
+  });
 
   if (mangaQuery.isError || mangaChaptersQuery.isError) return <p>Error</p>;
   if (mangaQuery.isLoading || mangaChaptersQuery.isLoading)
@@ -181,18 +265,23 @@ const SeriesPage = () => {
     <div className="flex flex-col gap-4 p-2">
       <p className="text-center text-2xl">{manga.englishTitle}</p>
       <div className="grid grid-cols-1 place-items-center md:grid-cols-3 md:place-items-start">
-        <img
-          className="shadow-xl"
-          src={pb.getFileUrl(manga, manga.coverExtraLarge)}
-          alt=""
-        />
+        <div className="flex w-full items-center justify-center">
+          <img
+            className="shadow-xl"
+            src={pb.getFileUrl(manga, manga.coverExtraLarge)}
+            alt=""
+          />
+        </div>
         <div className="col-span-2 flex flex-col gap-2 p-2 md:items-start">
           <p
             className={`whitespace-pre-wrap ${collapsed ? "line-clamp-6" : ""}`}
           >
             {parse(manga.description)}
           </p>
-          <button onClick={() => setCollapsed((prev) => !prev)}>
+          <button
+            className="text-gray-500 dark:text-gray-300"
+            onClick={() => setCollapsed((prev) => !prev)}
+          >
             {collapsed ? "Show more" : "Show less"}
           </button>
         </div>
@@ -230,6 +319,14 @@ const SeriesPage = () => {
               Remove Manga
             </button>
           )}
+          <button
+            className="rounded bg-red-300 px-4 py-2 disabled:bg-green-300"
+            onClick={() => {
+              if (allChapterIds.data) setSelectedItems(allChapterIds.data);
+            }}
+          >
+            Mark all chapters
+          </button>
         </div>
         <p>Chapters Available: {manga.chaptersAvailable}</p>
         <div className="grid grid-cols-2 gap-2 md:grid-cols-4 lg:grid-cols-5">
@@ -253,6 +350,24 @@ const SeriesPage = () => {
 
                   const isContinue = lastChapterRead.data?.chapter === item.id;
 
+                  const select = (select: boolean) => {
+                    if (!auth.user) {
+                      return;
+                    }
+
+                    console.log("Select");
+                    if (select) {
+                      setSelectedItems((prev) => [...prev, item.id]);
+                    } else {
+                      setSelectedItems((prev) => [
+                        ...prev.filter((i) => i !== item.id),
+                      ]);
+                    }
+                  };
+
+                  const showSelectMarker = selectedItems.length > 0;
+                  const selected = !!selectedItems.find((i) => i === item.id);
+
                   if (isLastPage && isLastItem) {
                     return (
                       <Chapter
@@ -262,6 +377,10 @@ const SeriesPage = () => {
                         isContinue={isContinue}
                         hasRead={hasReadChapter}
                         isGroup={manga.isGroup}
+                        isSelected={selected}
+                        showSelectMarker={showSelectMarker}
+                        select={select}
+                        disableSelectMarker={!auth.user}
                       />
                     );
                   }
@@ -272,6 +391,10 @@ const SeriesPage = () => {
                       isContinue={isContinue}
                       hasRead={hasReadChapter}
                       isGroup={manga.isGroup}
+                      isSelected={selected}
+                      showSelectMarker={showSelectMarker}
+                      select={select}
+                      disableSelectMarker={!auth.user}
                     />
                   );
                 })}
@@ -279,6 +402,42 @@ const SeriesPage = () => {
             );
           })}
         </div>
+
+        {selectedItems.length > 0 && (
+          <div className="fixed bottom-10 left-1/2 flex -translate-x-1/2 items-center rounded bg-red-300">
+            <button onClick={() => setSelectedItems([])}>
+              <XMarkIcon className="h-7 w-7" />
+            </button>
+            <p>{selectedItems.length} item(s) selected</p>
+            {selectedItems.length === 1 && (
+              <button>
+                <BookOpenIcon className="h-7 w-7" />
+              </button>
+            )}
+            <button
+              onClick={() => {
+                const items = selectedItems.filter((id) => {
+                  if (chapterRead.data) {
+                    return !chapterRead.data.find((i) => i.chapter === id);
+                  } else {
+                    return false;
+                  }
+                });
+
+                markItems.mutate({ items, markAsRead: true });
+              }}
+            >
+              <BookmarkIcon className="h-7 w-7" />
+            </button>
+            <button
+              onClick={() => {
+                markItems.mutate({ items: selectedItems, markAsRead: false });
+              }}
+            >
+              <BookmarkSlashIcon className="h-7 w-7" />
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
