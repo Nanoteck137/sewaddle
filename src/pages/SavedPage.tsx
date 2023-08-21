07ed1dfca6afefce243286f8159a44e87e3ec8db
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-08-21 20:30:20 +0200

    Cleanup

--- src/pages/SavedPage.tsx
+++ src/pages/SavedPage.tsx
@@ -1,23 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
-import { Navigate } from "react-router-dom";
-import { z } from "zod";
-
 import { useUserSavedMangas } from "../api";
-import { pb } from "../api/pocketbase";
 import MangaList from "../components/MangaList";
 import { useAuth } from "../contexts/AuthContext";
-import { Collection } from "../models/collection";
-import { MangaView } from "../models/manga";
-
-const Schema = z.array(
-  Collection.extend({
-    user: z.string(),
-    manga: z.string(),
-    expand: z.object({
-      manga: MangaView,
-    }),
-  }),
-);
 
 const SavedPage = () => {
   const auth = useAuth();
