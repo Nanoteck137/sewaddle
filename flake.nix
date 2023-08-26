f79d0ff7a2fc919c69c3c74bc649189c7c21c2ff
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-08-26 13:38:13 +0200

    Testing

--- flake.nix
+++ flake.nix
@@ -6,9 +6,11 @@
     flake-utils.url = "github:numtide/flake-utils";
     bp.url = "github:serokell/nix-npm-buildpackage";
     bp.inputs.nixpkgs.follows = "nixpkgs";
+    gitignore.url = "github:hercules-ci/gitignore.nix";
+    gitignore.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, flake-utils, nixpkgs, bp }: 
+  outputs = { self, flake-utils, nixpkgs, bp, gitignore }: 
     flake-utils.lib.eachDefaultSystem (system: 
       let 
         pkgs = import nixpkgs { inherit system; overlays = [ bp.overlays.default ]; };
@@ -18,11 +20,21 @@
 
         nodeDeps = node2nixOutput.nodeDependencies;
 
-        app = pkgs.buildNpmPackage {
+        test = { test ? "" }: pkgs.buildNpmPackage {
           name = "sewaddle";
           version = "0.0.1";
-          src = ./.;
+          src = gitignore.lib.gitignoreSource ./.;
           npmBuild = "npm run build";
+
+          extraEnvVars = {
+            VITE_TEST = test;
+          };
+
+          # buildPhase = ''
+          #   runHook preBuild
+          #   ${npmBuild}
+          #   runHook postBuild
+          # '';
 
           installPhase = ''
             runHook preInstall
@@ -31,49 +43,8 @@
           '';
         };
 
-        # app = pkgs.stdenv.mkDerivation {
-        #   name = "sewaddle";
-        #   version = "0.0.1";
-        #   src = ./.;
-        #   buildInputs = [ nodejs ];
-        #   buildPhase = ''
-        #     runHook preBuild
-        #     # symlink the generated node deps to the current directory for building
-        #     ln -sf ${nodeDeps}/lib/node_modules ./node_modules
-        #     export PATH="${nodeDeps}/bin:$PATH"
-        #     npm run build
-        #     runHook postBuild
-        #   '';
-        #   installPhase = ''
-        #     runHook preInstall
-        #     cp -r dist $out/
-        #     runHook postInstall
-        #   '';
-        # };
-
-        nginx = let
-          in
-          pkgs.dockerTools.buildLayeredImage {
-            name = "sewaddle";
-            tag = "latest";
-            contents = [
-              pkgs.caddy
-              app
-            ];
-
-            extraCommands = ''
-            '';
-
-            config = {
-              Cmd = [ "caddy" "file-server" "--listen" ":1337" "--root" "${app}" ];
-              ExposedPorts = {
-                "1337/tcp" = {};
-              };
-            };
-          };
       in with pkgs; {
-        packages.dockerImage = nginx;
-        packages.default = app;
+        lib.test = test;
         devShells.default = mkShell { buildInputs = [ nodejs node2nix ]; };
       }
     );
--- src/api/pocketbase.ts
+++ src/api/pocketbase.ts
@@ -1,4 +1,4 @@
 import PocketBase from "pocketbase";
 
 // TODO(patrik): Change URL to env variable
-export const pb = new PocketBase("http://10.28.28.5:8090");
+export const pb = new PocketBase(import.meta.env.VITE_TEST);
