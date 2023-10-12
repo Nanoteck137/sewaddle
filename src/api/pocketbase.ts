1c72220fe7279d2da3602b42f834d7210695e9ca
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-10-12 02:13:58 +0200

    Testing new pocketbase url

--- src/api/pocketbase.ts
+++ src/api/pocketbase.ts
@@ -1,4 +1,5 @@
 import PocketBase from "pocketbase";
 
 // TODO(patrik): Change URL to env variable
-export const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_BASE_URL);
+// export const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_BASE_URL);
+export const pb = new PocketBase(window.location.origin);
