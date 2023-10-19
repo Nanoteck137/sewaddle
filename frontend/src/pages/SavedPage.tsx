0904a1e114acfd3d7f0289bf96c91178f8bc5ce0
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-10-19 20:36:40 +0200

    Frontend: Removing old backend code

--- frontend/src/pages/SavedPage.tsx
+++ frontend/src/pages/SavedPage.tsx
@@ -1,25 +1,29 @@
 import { Navigate } from "react-router-dom";
 
-import { useUserSavedMangas } from "../api";
-import MangaList from "../components/MangaList";
 import { useAuth } from "../contexts/AuthContext";
 
 const SavedPage = () => {
   const auth = useAuth();
   if (!auth.isLoggedIn) return <Navigate to="/" />;
 
-  const userSavedMangas = useUserSavedMangas({ user: auth.user });
-
-  if (userSavedMangas.isError) return <p>Error</p>;
-  if (userSavedMangas.isLoading) return <p>Loading...</p>;
-
-  const mangaList = userSavedMangas.data.items.map((i) => i.expand.manga);
-
   return (
-    <div>
-      <MangaList list={mangaList} />
-    </div>
+    <p className="font-bold text-xl grid place-items-center">
+      Not implemented yet
+    </p>
   );
+
+  // const userSavedMangas = useUserSavedMangas({ user: auth.user });
+
+  // if (userSavedMangas.isError) return <p>Error</p>;
+  // if (userSavedMangas.isLoading) return <p>Loading...</p>;
+
+  // const mangaList = userSavedMangas.data.items.map((i) => i.expand.manga);
+
+  // return (
+  //   <div>
+  //     <MangaList list={mangaList} />
+  //   </div>
+  // );
 };
 
 export default SavedPage;
