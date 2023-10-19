0b628d15805eb559d2d2a4917723b58583c66bd9
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-10-19 20:28:31 +0200

    Backend: Implmented user password changing

--- backend/src/api/auth/router.ts
+++ backend/src/api/auth/router.ts
@@ -34,6 +34,7 @@ export const authRouter = router({
     )
     .mutation(async ({ input }) => {
       if (input.password !== input.passwordConfirm) {
+        // TODO(patrik): Add message
         throw new TRPCError({ code: "BAD_REQUEST" });
       }
 
@@ -41,6 +42,32 @@ export const authRouter = router({
         username: input.username,
         password: input.password,
       });
+    }),
+  changePassword: protectedProcedure
+    .input(
+      z.object({
+        oldPassword: z.string(),
+        newPassword: z.string().min(1),
+        newPasswordConfirm: z.string().min(1),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      if (input.newPassword !== input.newPasswordConfirm) {
+        // TODO(patrik): Add message
+        throw new TRPCError({ code: "BAD_REQUEST" });
+      }
+
+      if (ctx.user.password !== input.oldPassword) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Old password mismatch",
+        });
+      }
+
+      await db
+        .update(users)
+        .set({ password: input.newPassword })
+        .where(eq(users.id, ctx.user.id));
     }),
   getProfile: protectedProcedure.query(async ({ ctx }) => {
     return {
