be33e1525911137b2aa04fa2c708c33354ba74c6
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-10-26 12:46:20 +0200

    Backend: Added automatic migration on startup for production

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,7 +1,12 @@
 import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
-import { getDatabaseFile } from "./env";
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
+import { env, getDatabaseFile } from "./env";
 import * as schema from "./schema";
 
 const sqlite = new Database(getDatabaseFile());
 export const db = drizzle(sqlite, { schema });
+
+if (env.NODE_ENV === "production") {
+  migrate(db, { migrationsFolder: "./migrations" });
+}
