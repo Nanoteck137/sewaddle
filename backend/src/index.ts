7e644f2db76c3b5498eb70878977bb9c16749660
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-11-02 22:43:25 +0100

    Backend: Removed helmet when not running in production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -50,17 +50,19 @@ function createContextInner(token: string | null): Context {
 
 const app = express();
 
-app.use(cors({}));
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        imgSrc: ["'self'", "https://placehold.co/"],
-        scriptSrc: ["'self'"],
+app.use(cors());
+if (env.NODE_ENV === "production") {
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          imgSrc: ["'self'", "https://placehold.co/"],
+          scriptSrc: ["'self'"],
+        },
       },
-    },
-  }),
-);
+    }),
+  );
+}
 app.use(morgan(env.NODE_ENV === "development" ? "dev" : "combined"));
 app.use(express.static(path.join(process.cwd(), "public")));
 
