cd486b8c2d74c223c2ab3e6cd70de4d8c7430748
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-11-02 15:31:42 +0100

    Frontend: Commenting out the old header code

--- frontend/src/layouts/DefaultLayout.tsx
+++ frontend/src/layouts/DefaultLayout.tsx
@@ -20,27 +20,26 @@ import {
 } from "@heroicons/react/24/outline";
 import { useState } from "react";
 import { useTernaryDarkMode } from "usehooks-ts";
-import { FloatingSidebar, Sidebar } from "../components/sidebar/Sidebar";
 
-const Header = () => {
-  return (
-    <div className="fixed left-0 right-0 z-50 h-16 border-b-2 bg-white px-4 shadow-lg dark:border-gray-600 dark:bg-gray-700">
-      <div className="flex h-full items-center justify-between">
-        <div className="flex h-full items-center gap-4 px-3">
-          <FloatingSidebar />
+// const Header = () => {
+//   return (
+//     <div className="fixed left-0 right-0 z-50 h-16 border-b-2 bg-white px-4 shadow-lg dark:border-gray-600 dark:bg-gray-700">
+//       <div className="flex h-full items-center justify-between">
+//         <div className="flex h-full items-center gap-4 px-3">
+//           <FloatingSidebar />
 
-          <Link to="/" className="text-2xl">
-            Sewaddle
-          </Link>
-        </div>
+//           <Link to="/" className="text-2xl">
+//             Sewaddle
+//           </Link>
+//         </div>
 
-        <button>
-          <MagnifyingGlassIcon className="h-8 w-8" />
-        </button>
-      </div>
-    </div>
-  );
-};
+//         <button>
+//           <MagnifyingGlassIcon className="h-8 w-8" />
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
 
 const AccountDropdown = () => {
   const auth = useAuth();
@@ -318,18 +317,18 @@ const NewHeader = () => {
   );
 };
 
-const DefaultLayout = () => {
-  return (
-    <div className="flex h-screen">
-      <Header />
-      <Sidebar />
+// const DefaultLayout = () => {
+//   return (
+//     <div className="flex h-screen">
+//       <Header />
+//       <Sidebar />
 
-      <div className="z-40 mt-16 flex-grow lg:ml-24 xl:ml-60">
-        <Outlet />
-      </div>
-    </div>
-  );
-};
+//       <div className="z-40 mt-16 flex-grow lg:ml-24 xl:ml-60">
+//         <Outlet />
+//       </div>
+//     </div>
+//   );
+// };
 
 const NewDefaultLayout = () => {
   return (
