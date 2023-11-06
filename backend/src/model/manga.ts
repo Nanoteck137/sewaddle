cff5b2ae44f446880f784391d6cd60dfa1b0652f
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-11-06 15:03:16 +0100

    Backend: Updated manga metadata

--- backend/src/model/manga.ts
+++ backend/src/model/manga.ts
@@ -12,5 +12,12 @@ export const MangaMetadata = z.object({
   title: z.string(),
   cover: z.string(),
   chapters: z.array(ChapterMetadata),
+
+  description: z.string(),
+  anilistId: z.number(),
+  malId: z.number(),
+  status: z.enum(["RELEASING", "FINISHED", "HIATUS"]),
+  startDate: z.string(),
+  endDate: z.string(),
 });
 export type MangaMetadata = z.infer<typeof MangaMetadata>;
