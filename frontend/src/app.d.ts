38b4305ac55c75b6f82dba75d7f2d375471535d3
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-11-13 05:27:45 +0100

    Frontend: Boilerplate for sveltekit

---
+++ frontend/src/app.d.ts
@@ -0,0 +1,16 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+import { type User } from "$lib/schema";
+
+declare global {
+  namespace App {
+    // interface Error {}
+    interface Locals {
+      user?: User;
+    }
+    // interface PageData {}
+    // interface Platform {}
+  }
+}
+
+export {};
