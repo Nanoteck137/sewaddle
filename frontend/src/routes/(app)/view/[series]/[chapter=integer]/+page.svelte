46548af052a339dcc7e8c82eb3308993956b7bed
Patrik M. Rosenström <patrik.millvik@gmail.com>
2023-11-15 04:54:53 +0100

    Frontend: Removed unused import

--- frontend/src/routes/(app)/view/[series]/[chapter=integer]/+page.svelte
+++ frontend/src/routes/(app)/view/[series]/[chapter=integer]/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { browser } from "$app/environment";
   import { page } from "$app/stores";
-  import { onMount } from "svelte";
   // import { shortcut, type ShortcutTrigger } from "@svelte-put/shortcut";
 
   export let data;
