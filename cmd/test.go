4fce035c5bc1900fe4653647f1aad019850fbfc1
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-02-01 19:02:54 +0100

    Test code

---
+++ cmd/test.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	"github.com/nanoteck137/sewaddle/library"
+	"github.com/spf13/cobra"
+)
+
+var testCmd = &cobra.Command{
+	Use: "test",
+	Run: func(cmd *cobra.Command, args []string) {
+		godotenv.Load()
+
+		dbUrl := os.Getenv("DB_URL")
+		if dbUrl == "" {
+			log.Fatal("DB_URL not set")
+		}
+
+		db, err := pgxpool.New(context.Background(), dbUrl)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		lib, err := library.ReadFromDir("/Volumes/manga")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		lib.Sync(db)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(testCmd)
+}
