6f667cdf7d896b4f0b30a4cfb745561bdc4a7953
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-02-01 10:59:13 +0100

    Added nix flake for dev shell

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Devshell for sewaddle";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            air
+          ];
+        };
+      }
+    );
+}
