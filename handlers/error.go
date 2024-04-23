ca338764e82b9002b7eba3d5b97a88da04458e63
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-04-23 08:08:42 +0200

    Changed the global error handler

--- handlers/error.go
+++ handlers/error.go
@@ -1,21 +1,21 @@
 package handlers
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 	"github.com/nanoteck137/sewaddle/types"
 )
 
 func ErrorHandler(err error, c echo.Context) {
-	code := http.StatusInternalServerError
-
-	if apiError, ok := err.(*types.ApiError); ok {
-		code = apiError.Code
+	switch err := err.(type) {
+	case *types.ApiError:
+		c.JSON(err.Code, types.ApiResponse{
+			Status: types.StatusError,
+			Error:  err,
+		})
+	default:
+		c.JSON(500, types.ApiResponse{
+			Status: types.StatusError,
+			Error:  types.NewApiError(500, err.Error()),
+		})
 	}
-
-	c.JSON(code, map[string]any{
-		"error": err.Error(),
-	})
-
 }
