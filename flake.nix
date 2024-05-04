bcb1c6fb7422bb661d22f9186198144f64ceb92f
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-05-04 13:12:46 +0200

    Working on docker image

--- flake.nix
+++ flake.nix
@@ -15,16 +15,34 @@
           inherit system overlays;
         };
 
+        version = self.shortRev or "dirty";
+
         app = pkgs.buildGoModule {
           pname = "sewaddle";
-          version = self.shortRev or "dirty";
+          inherit version;
           src = ./.;
 
           vendorHash = "sha256-6kR1t22fJ0sLB2DjG+KdSN4mVD6n69c82zOonvfH3A8=";
         };
+
+        dockerImage = pkgs.dockerTools.buildLayeredImage {
+          name = "sewaddle";
+          tag = version;
+          config = {
+            Env = [
+              "SEWADDLE_DATA_DIR=/data"
+            ];
+            Entrypoint = ["${app}/bin/sewaddle"];
+            Cmd = [ "serve" ];
+            ExposedPorts = {
+              "3000/tcp" = {};
+            };
+          };
+        };
       in
       {
         packages.default = app;
+        packages.dockerImage = dockerImage;
 
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
