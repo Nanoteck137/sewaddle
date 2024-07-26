f8cbe05db8b3c0529994601e36b500d9221f97bc
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-07-26 16:02:50 +0200

    Removing sewaddle-core from libsync

--- database/libsync/library.go
+++ database/libsync/library.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/doug-martin/goqu/v9"
 	_ "github.com/doug-martin/goqu/v9/dialect/postgres"
-	"github.com/nanoteck137/sewaddle-core/library"
 	"github.com/nanoteck137/sewaddle/database"
 	"github.com/nanoteck137/sewaddle/types"
 	"github.com/nanoteck137/sewaddle/utils"
