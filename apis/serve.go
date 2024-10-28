7e7470e5d75b1d9927ff60ee191d9add030c81a1
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-10-28 15:51:29 +0100

    Removed test

--- apis/serve.go
+++ apis/serve.go
@@ -1,82 +1,17 @@
 package apis
 
 import (
-	"context"
-	"errors"
-	"fmt"
 	"net/http"
 	"os"
-	"sort"
 
-	"github.com/kr/pretty"
-	"github.com/maruel/natural"
 	"github.com/nanoteck137/pyrin"
 	"github.com/nanoteck137/sewaddle/assets"
 	"github.com/nanoteck137/sewaddle/core"
-	"github.com/nanoteck137/sewaddle/database"
 )
 
 func RegisterHandlers(app core.App, router pyrin.Router) {
 	g := router.Group("/api/v1")
 	InstallHandlers(app, g)
-
-	g.Register(pyrin.ApiHandler{
-		Name:        "Testing",
-		Method:      http.MethodPost,
-		Path:        "/test",
-		HandlerFunc: func(c pyrin.Context) (any, error) {
-			ctx := context.TODO()
-
-			db := app.DB()
-
-			serie, err := db.GetSerieByName(ctx, "Test Serie")
-			if err != nil {
-				if errors.Is(err, database.ErrItemNotFound) {
-					serie, err = db.CreateSerie(ctx, database.CreateSerieParams{
-						Name: "Test Serie",
-					})
-					if err != nil {
-						return nil, err
-					}
-				} else {
-					return nil, err
-				}
-			}
-
-			n := func(i any) {
-				db.CreateChapter(ctx, database.CreateChapterParams{
-					SerieSlug: serie.Slug,
-					Title:     fmt.Sprintf("Chapter %v", i),
-				})
-			}
-
-			for i := 0; i < 50; i++ {
-				n(i)
-			}
-
-			n("14.5")
-			n("14.4")
-			n("14.494")
-			n("2.5")
-			n("14.5")
-			n("14.5")
-
-			chapters, err := db.GetSerieChaptersById(ctx, serie.Slug)
-			if err != nil {
-				return nil, err
-			}
-
-			pretty.Println(chapters)
-
-			sort.SliceStable(chapters, func(i, j int) bool {
-				return natural.Less(chapters[i].Title, chapters[j].Title)
-			})
-
-			pretty.Println(chapters)
-
-			return nil, nil
-		},
-	})
 
 	g = router.Group("/files")
 	g.Register(
