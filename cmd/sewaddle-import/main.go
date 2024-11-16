b9be87ff22e018e1f19710da2e88c15ee431438d
Patrik M. Rosenström <patrik.millvik@gmail.com>
2024-11-17 00:33:57 +0100

    Working on new import process

--- cmd/sewaddle-import/main.go
+++ cmd/sewaddle-import/main.go
@@ -1,41 +1,66 @@
 package main
 
 import (
-	"archive/zip"
 	"context"
-	"database/sql"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"io"
-	"io/fs"
 	"os"
 	"path"
-	"path/filepath"
 	"strconv"
 	"strings"
 
 	"github.com/kr/pretty"
 	"github.com/mattn/go-sqlite3"
-	"github.com/nanoteck137/packer/metadata"
 	"github.com/nanoteck137/sewaddle"
 	"github.com/nanoteck137/sewaddle/core/log"
 	"github.com/nanoteck137/sewaddle/database"
 	"github.com/nanoteck137/sewaddle/types"
-	"github.com/nanoteck137/sewaddle/utils"
 	"github.com/spf13/cobra"
 )
 
 var AppName = sewaddle.AppName + "-import"
 
 var rootCmd = &cobra.Command{
-	Use:     AppName + " <OUT>",
+	Use:     AppName,
 	Version: sewaddle.Version,
-	Args:    cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		out := args[0]
-		dir, _ := cmd.Flags().GetString("dir")
+}
 
+type MangaInfoChapter struct {
+	Index int      `json:"index"`
+	Name  string   `json:"name"`
+	Pages []string `json:"pages"`
+}
+
+type MangaInfo struct {
+	Title    string             `json:"title"`
+	Cover    string             `json:"cover"`
+	Chapters []MangaInfoChapter `json:"chapters"`
+}
+
+func ReadMangaInfo(p string) (MangaInfo, error) {
+	d, err := os.ReadFile(p)
+	if err != nil {
+		return MangaInfo{}, err
+	}
+
+	var res MangaInfo
+	err = json.Unmarshal(d, &res)
+	if err != nil {
+		return MangaInfo{}, err
+	}
+
+	return res, nil
+}
+
+var importCmd = &cobra.Command{
+	Use:  "old-format <INPUT>",
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		input := args[0]
+
+		// TODO(patrik): Temp
+		out := "./work"
 		workDir := types.WorkDir(out)
 
 		dirs := []string{
@@ -61,282 +86,129 @@ var rootCmd = &cobra.Command{
 
 		ctx := context.TODO()
 
-		var series []string
-		var chapters []string
+		_ = ctx
 
-		filepath.Walk(dir, func(p string, info fs.FileInfo, err error) error {
-			name := info.Name()
-			ext := path.Ext(name)
+		mangaInfo, err := ReadMangaInfo(path.Join(input, "manga.json"))
+		if err != nil {
+			log.Fatal("Failed", err)
+		}
 
-			switch ext {
-			case ".sw":
-				chapters = append(chapters, p)
-			case ".sws":
-				series = append(series, p)
-			}
+		pretty.Println(mangaInfo)
+		serie, err := db.GetSerieByName(ctx, mangaInfo.Title)
+		if err != nil {
+			if errors.Is(err, database.ErrItemNotFound) {
+				serie, err = db.CreateSerie(ctx, database.CreateSerieParams{
+					Name: mangaInfo.Title,
+				})
+				if err != nil {
+					log.Fatal("Failed", "err", err)
+				}
 
-			return nil
-		})
+				serieDir := workDir.SerieDir(serie.Slug)
+				dirs := []string{
+					serieDir.String(),
+					serieDir.ChaptersDir(),
+					serieDir.ImagesDir(),
+				}
 
-		fmt.Printf("chapters: %v\n", chapters)
-		fmt.Printf("series: %v\n", series)
-
-		affectedSeries := make(map[string]struct{})
-
-		for _, p := range series {
-			r, err := zip.OpenReader(p)
-			if err != nil {
-				log.Fatal("Failed to open series", "err", err)
-			}
-			defer r.Close()
-
-			files := make(map[string]*zip.File)
-
-			for _, f := range r.File {
-				files[f.Name] = f
-				fmt.Printf("f.Name: %v\n", f.Name)
-			}
-
-			infoFile, exists := files["info.json"]
-			if !exists {
-				log.Fatal("Missing info.json")
-			}
-
-			infoReader, err := infoFile.Open()
-			if err != nil {
+				for _, dir := range dirs {
+					err := os.Mkdir(dir, 0755)
+					if err != nil && !os.IsExist(err) {
+						log.Fatal("Failed to create serie directory", "err", err, "serieSlug", serie.Slug)
+					}
+				}
+			} else {
 				log.Fatal("Failed", "err", err)
 			}
-			defer infoReader.Close()
+		}
 
-			var info metadata.SeriesInfo
+		pretty.Println(serie)
 
-			decoder := json.NewDecoder(infoReader)
-			err = decoder.Decode(&info)
-			if err != nil {
-				log.Fatal("Failed", "err", err)
-			}
+		serieDir := workDir.SerieDir(serie.Slug)
 
-			pretty.Println(info)
+		for _, c := range mangaInfo.Chapters {
+			func() {
+				p := path.Join(input, "chapters", strconv.Itoa(c.Index))
 
-			serie, err := db.GetSerieByName(ctx, info.Name)
-			if err != nil {
-				if errors.Is(err, database.ErrItemNotFound) {
-					serie, err = db.CreateSerie(ctx, database.CreateSerieParams{
-						Name: info.Name,
-					})
-					if err != nil {
-						log.Fatal("Failed", "err", err)
-					}
+				name := strings.TrimSpace(c.Name)
 
-					serieDir := workDir.SerieDir(serie.Slug)
-					dirs := []string{
-						serieDir.String(),
-						serieDir.ChaptersDir(),
-						serieDir.ImagesDir(),
-					}
+				// TODO(patrik): Validate the whole manga info
+				if name == "" {
+					log.Fatal("Name can't be empty")
+				}
 
-					for _, dir := range dirs {
-						err := os.Mkdir(dir, 0755)
-						if err != nil && !os.IsExist(err) {
-							log.Fatal("Failed to create serie directory", "err", err, "serieSlug", serie.Slug)
+				chapter, err := db.CreateChapter(ctx, database.CreateChapterParams{
+					SerieSlug: serie.Slug,
+					Title:     c.Name,
+				})
+
+				if err != nil {
+					pretty.Println(err)
+					var sqlErr sqlite3.Error
+					if errors.As(err, &sqlErr) {
+						if sqlErr.ExtendedCode == sqlite3.ErrConstraintPrimaryKey {
+							return
 						}
 					}
-				} else {
-					log.Fatal("Failed", "err", err)
+
+					log.Fatal("Failed to create chapter", "err", err)
 				}
-			}
 
-			serieDir := workDir.SerieDir(serie.Slug)
+				chapterDir := serieDir.ChapterDir(chapter.Slug)
 
-			{
-				f := files[info.Cover.Small]
-				r, err := f.Open()
-				if err != nil {
-					log.Fatal("Failed", "err", err)
+				err = os.Mkdir(chapterDir, 0755)
+				if err != nil && !os.IsExist(err) {
+					log.Fatal("Failed to create chapater directory", "err", err, "serieSlug", serie.Slug, "chapterSlug", chapter.Slug)
 				}
-				defer r.Close()
 
+				var names []string
+				for i, page := range c.Pages {
+					p := path.Join(p, page)
+					r, err := os.Open(p)
+					if err != nil {
+						log.Fatal("Failed to open page file for reading", "err", err)
+					}
 
-				dst, err := os.Create(path.Join(serieDir.ImagesDir(), "cover-small.png"))
-				if err != nil {
-					log.Fatal("Failed", "err", err)
+					dstName := strconv.Itoa(i) + path.Ext(p)
+					d := path.Join(chapterDir, dstName)
+					dst, err := os.OpenFile(d, os.O_WRONLY|os.O_CREATE, 0644)
+					if err != nil {
+						log.Fatal("Failed to open page file for writing", "err", err)
+					}
+					defer dst.Close()
+
+					_, err = io.Copy(dst, r)
+					if err != nil {
+						log.Fatal("Failed to copy page content to file", "err", err)
+					}
+
+					names = append(names, dstName)
 				}
-				defer dst.Close()
 
-				_, err = io.Copy(dst, r)
-				if err != nil {
-					log.Fatal("Failed", "err", err)
-				}
-			}
-
-			err = db.UpdateSerie(ctx, serie.Slug, database.SerieChanges{
-				Cover: types.Change[sql.NullString]{
-					Value: sql.NullString{
-						String: "cover-small.png",
-						Valid:  true,
+				err = db.UpdateChapter(ctx, serie.Slug, chapter.Slug, database.ChapterChanges{
+					Pages: types.Change[string]{
+						Value:   strings.Join(names, ","),
+						Changed: true,
 					},
-					Changed: true,
-				},
-			})
-			if err != nil {
-				log.Fatal("Failed", "err", err)
-			}
+				})
 
-			pretty.Print(serie)
+				if err != nil {
+					log.Fatal("Failed to update chapter", "err", err)
+				}
+			}()
 		}
 
-		for _, p := range chapters {
-			r, err := zip.OpenReader(p)
-			if err != nil {
-				log.Fatal("Failed to open chapter", "err", err)
-			}
-			defer r.Close()
-
-			files := make(map[string]*zip.File)
-
-			for _, f := range r.File {
-				files[f.Name] = f
-				fmt.Printf("f.Name: %v\n", f.Name)
-			}
-
-			infoFile, exists := files["info.json"]
-			if !exists {
-				log.Fatal("Missing info.json")
-			}
-
-			infoReader, err := infoFile.Open()
-			if err != nil {
-				log.Fatal("Failed", "err", err)
-			}
-			defer infoReader.Close()
-
-			var info metadata.EntryInfo
-
-			decoder := json.NewDecoder(infoReader)
-			err = decoder.Decode(&info)
-			if err != nil {
-				log.Fatal("Failed", "err", err)
-			}
-
-			serie, err := db.GetSerieByName(ctx, info.Series)
-			fmt.Printf("err: %v\n", err)
-			if err != nil {
-				if errors.Is(err, database.ErrItemNotFound) {
-					serie, err = db.CreateSerie(ctx, database.CreateSerieParams{
-						Name: info.Series,
-					})
-
-					if err != nil {
-						log.Fatal("Failed", "err", err)
-					}
-
-					serieDir := workDir.SerieDir(serie.Slug)
-					dirs := []string{
-						serieDir.String(),
-						serieDir.ChaptersDir(),
-						serieDir.ImagesDir(),
-					}
-
-					for _, dir := range dirs {
-						err := os.Mkdir(dir, 0755)
-						if err != nil && !os.IsExist(err) {
-							log.Fatal("Failed to create serie directory", "err", err, "serieSlug", serie.Slug)
-						}
-					}
-				} else {
-					log.Fatal("Failed", "err", err)
-				}
-			}
-
-			fmt.Printf("serie: %v\n", serie)
-
-			chapter, err := db.CreateChapter(ctx, database.CreateChapterParams{
-				SerieSlug: serie.Slug,
-				Title:     info.Name,
-				// Number: sql.NullInt64{
-				// 	Int64: data.Number,
-				// 	Valid: data.Number != 0,
-				// },
-			})
-
-			if err != nil {
-				pretty.Println(err)
-				var sqlErr sqlite3.Error
-				if errors.As(err, &sqlErr) {
-					// TODO(patrik): Let chapters re-import 
-					if sqlErr.ExtendedCode == sqlite3.ErrConstraintPrimaryKey {
-						continue
-					}
-				}
-
-				log.Fatal("Failed to create chapter", "err", err)
-			}
-
-			serieDir := workDir.SerieDir(serie.Slug)
-			chapterDir := serieDir.ChapterDir(chapter.Slug)
-
-			err = os.Mkdir(chapterDir, 0755)
-			if err != nil && !os.IsExist(err) {
-				log.Fatal("Failed to create chapater directory", "err", err, "serieSlug", serie.Slug, "chapterSlug", chapter.Slug)
-			}
-
-			var names []string
-
-			for _, p := range info.Pages {
-				file, exists := files[p]
-				if !exists {
-					log.Fatal("Page not found")
-				}
-
-				r, err := file.Open()
-				if err != nil {
-					log.Fatal("Failed to open page file", "err", err)
-				}
-				defer r.Close()
-
-				n := utils.ExtractNumber(file.Name)
-				dstName := strconv.Itoa(n) + path.Ext(file.Name)
-				d := path.Join(chapterDir, dstName)
-				dst, err := os.OpenFile(d, os.O_RDWR|os.O_CREATE, 0644)
-				if err != nil {
-					log.Fatal("Failed to open file for page", "err", err)
-				}
-				defer dst.Close()
-
-				_, err = io.Copy(dst, r)
-				if err != nil {
-					log.Fatal("Failed to copy page content to file", "err", err)
-				}
-
-				names = append(names, dstName)
-			}
-
-			err = db.UpdateChapter(ctx, serie.Slug, chapter.Slug, database.ChapterChanges{
-				Pages: types.Change[string]{
-					Value:   strings.Join(names, ","),
-					Changed: true,
-				},
-			})
-
-			if err != nil {
-				log.Fatal("Failed to update chapter", "err", err)
-			}
-
-			affectedSeries[serie.Slug] = struct{}{}
-		}
-
-		for k := range affectedSeries {
-			err := db.RecalculateNumberForSerie(ctx, k)
-			if err != nil {
-				log.Fatal("Failed", "err", err)
-			}
+		err = db.RecalculateNumberForSerie(ctx, serie.Slug)
+		if err != nil {
+			log.Fatal("Failed", err)
 		}
 	},
 }
 
 func init() {
 	rootCmd.SetVersionTemplate(sewaddle.VersionTemplate(AppName))
-	rootCmd.Flags().String("dir", ".", "Search directory")
+
+	rootCmd.AddCommand(importCmd)
 }
 
 func Execute() {
