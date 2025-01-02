ca05111747fc3a3267c060d98cd1d5f082218ce0
Patrik M. Rosenström <patrik.millvik@gmail.com>
2025-01-02 19:00:49 +0100

    Fixing the series page

--- web/src/routes/(app)/series/+page.svelte
+++ web/src/routes/(app)/series/+page.svelte
@@ -7,30 +7,23 @@
     class="grid grid-cols-2 gap-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7"
   >
     {#each data.series as serie}
-      <div class="flex flex-col items-center">
-        <div class="group">
-          <a class="relative" href={`/series/${serie.id}`}>
-            <img
-              class="h-56 w-40 rounded object-cover group-hover:brightness-75"
-              loading="lazy"
-              src={serie.coverArt.medium}
-              alt={`${serie.name} Cover`}
-            />
+      <div class="group flex flex-col items-center">
+        <a href={`/series/${serie.id}`} title={serie.name}>
+          <img
+            class="h-[206px] max-w-[160px] rounded border object-cover group-hover:brightness-75 group-active:scale-95"
+            loading="lazy"
+            src={serie.coverArt.medium}
+            alt="cover"
+          />
 
-            <div
-              class="absolute bottom-0 left-0 right-0 top-0 bg-purple-400/70"
-            ></div>
-          </a>
           <div class="h-2"></div>
-          <a
-            class="line-clamp-2 w-40 text-sm font-medium group-hover:underline"
-            title={serie.name}
-            href={`/series/${serie.id}`}
+
+          <p
+            class="line-clamp-2 w-40 text-center text-xs group-hover:underline"
           >
             {serie.name}
-          </a>
-        </div>
-        <div class="h-2"></div>
+          </p>
+        </a>
       </div>
     {/each}
   </div>
--- web/src/routes/(app)/series/[id]/+page.svelte
+++ web/src/routes/(app)/series/[id]/+page.svelte
@@ -8,6 +8,7 @@
     buttonVariants,
     Checkbox,
     DropdownMenu,
+    Separator,
   } from "@nanoteck137/nano-ui";
   import {
     Bookmark,
@@ -30,6 +31,7 @@
   type Chapter = (typeof data.chapters)[number];
 
   let selectedChapters = $state<string[]>([]);
+  let isEditing = $derived(selectedChapters.length > 0);
 
   function isSelected(chapterId: string) {
     for (let i = 0; i < selectedChapters.length; i++) {
@@ -49,7 +51,9 @@
     alt=""
   />
 
-  <p class="line-clamp-2 text-center font-bold">{data.serie.name}</p>
+  <p class="line-clamp-2 text-center text-sm font-semibold">
+    {data.serie.name}
+  </p>
 
   {#if data.serie.user?.bookmark?.chapterId}
     <Button
@@ -61,28 +65,74 @@
   {/if}
 </div>
 
-<div class="h-4"></div>
+<div class="h-2"></div>
+<Separator />
+
+<div class="flex justify-between px-2 py-2 text-xs">
+  <p>Chapters</p>
+  <p>{data.chapters.length} chapter(s)</p>
+</div>
 
 <div>
   <div class={`flex flex-col gap-2 ${selectedChapters.length > 0 ? "" : ""}`}>
-    {#snippet normalChapter(chapter: Chapter)}
-      <div class="group relative flex items-center gap-2 border-b pb-1 pr-4">
+    {#each data.chapters as chapter, i}
+      <div class="group relative flex items-center gap-2 px-2">
+        {#if isEditing}
+          <!-- svelte-ignore a11y_consider_explicit_label -->
+          <button
+            class="absolute inset-0"
+            onclick={(e) => {
+              if (e.shiftKey) {
+                const firstSelected =
+                  selectedChapters.length <= 0 ? 0 : selectedChapters[0];
+                let first = data.chapters.findIndex(
+                  (i) => i.id === firstSelected,
+                );
+
+                let last = i;
+                if (first > last) {
+                  const tmp = last;
+                  last = first;
+                  first = tmp;
+                }
+
+                const items = [];
+                const numItems = last - first + 1;
+                for (let i = 0; i < numItems; i++) {
+                  items.push(first + i);
+                }
+
+                const ids = items.map((i) => data.chapters[i].id);
+                selectedChapters = ids;
+              } else {
+                if (isSelected(chapter.id)) {
+                  selectedChapters = selectedChapters.filter(
+                    (id) => id !== chapter.id,
+                  );
+                } else {
+                  selectedChapters.push(chapter.id);
+                }
+              }
+            }}
+          ></button>
+        {/if}
         <div class="flex flex-grow items-center gap-2">
           <img
-            class="h-14 w-10 rounded object-cover"
+            class="h-[51px] w-[40px] rounded object-cover"
             loading="lazy"
             src={chapter.coverArt.small}
-            alt="Chapter Cover"
+            alt="cover"
           />
           <div class="flex flex-col gap-1">
             <a
-              class="line-clamp-1 flex items-center gap-2 font-medium"
+              class="line-clamp-1 flex items-center gap-2 text-sm"
               title={chapter.name}
               href={`/view/${chapter.id}/scroll`}
             >
               {#if data.serie.user?.bookmark?.chapterId === chapter.id}
                 <Check size={14} />
               {/if}
+
               {chapter.name}
             </a>
             {#if chapter.user?.isMarked}
@@ -92,173 +142,109 @@
         </div>
 
         <div class="flex items-center gap-2">
-          <DropdownMenu.Root>
-            <DropdownMenu.Trigger
-              class={cn(
-                buttonVariants({ variant: "ghost", size: "icon" }),
-                "rounded-full",
-              )}
-            >
-              <EllipsisVertical />
-            </DropdownMenu.Trigger>
-            <DropdownMenu.Content align="end">
-              <DropdownMenu.Group>
-                <DropdownMenu.Item
-                  onSelect={async () => {
-                    goto(`/view/${chapter.id}/scroll`);
-                  }}
-                >
-                  <BookOpen />
-                  Read Chapter
-                </DropdownMenu.Item>
-
-                {#if data.user}
-                  {#if !chapter.user?.isMarked}
-                    <DropdownMenu.Item
-                      onSelect={async () => {
-                        const res = await apiClient.markChapters({
-                          chapters: [chapter.id],
-                        });
-                        if (!res.success) {
-                          toast.error("Error: " + formatError(res.error));
-                          console.error(formatError(res.error));
-                        }
-
-                        await invalidateAll();
-                      }}
-                    >
-                      <BookPlus />
-                      Mark as Read
-                    </DropdownMenu.Item>
-                  {:else}
-                    <DropdownMenu.Item
-                      onSelect={async () => {
-                        const res = await apiClient.unmarkChapters({
-                          chapters: [chapter.id],
-                        });
-                        if (!res.success) {
-                          toast.error("Error: " + formatError(res.error));
-                          console.error(formatError(res.error));
-                        }
-
-                        await invalidateAll();
-                      }}
-                    >
-                      <BookMinus />
-                      Mark as not Read
-                    </DropdownMenu.Item>
-                  {/if}
-                {/if}
-
-                {#if data.user}
+          {#if !isEditing}
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger
+                class={cn(
+                  buttonVariants({ variant: "ghost", size: "icon" }),
+                  "rounded-full",
+                )}
+              >
+                <EllipsisVertical />
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content align="end">
+                <DropdownMenu.Group>
                   <DropdownMenu.Item
                     onSelect={async () => {
-                      const res = await apiClient.updateBookmark({
-                        serieId: data.serie.id,
-                        chapterId: chapter.id,
-                        page: 0,
-                      });
-                      if (!res.success) {
-                        toast.error("Error: " + formatError(res.error));
-                        console.error(formatError(res.error));
-                      }
-
-                      await invalidateAll();
+                      goto(`/view/${chapter.id}/scroll`);
                     }}
                   >
-                    <Bookmark />
-                    Set as Bookmark
+                    <BookOpen />
+                    Read Chapter
                   </DropdownMenu.Item>
-                {/if}
-              </DropdownMenu.Group>
-            </DropdownMenu.Content>
-          </DropdownMenu.Root>
 
-          <Checkbox
-            checked={false}
-            onCheckedChange={() => {
-              selectedChapters.push(chapter.id);
-            }}
-          />
+                  {#if data.user}
+                    {#if !chapter.user?.isMarked}
+                      <DropdownMenu.Item
+                        onSelect={async () => {
+                          const res = await apiClient.markChapters({
+                            chapters: [chapter.id],
+                          });
+                          if (!res.success) {
+                            toast.error("Error: " + formatError(res.error));
+                            console.error(formatError(res.error));
+                          }
+
+                          await invalidateAll();
+                        }}
+                      >
+                        <BookPlus />
+                        Mark as Read
+                      </DropdownMenu.Item>
+                    {:else}
+                      <DropdownMenu.Item
+                        onSelect={async () => {
+                          const res = await apiClient.unmarkChapters({
+                            chapters: [chapter.id],
+                          });
+                          if (!res.success) {
+                            toast.error("Error: " + formatError(res.error));
+                            console.error(formatError(res.error));
+                          }
+
+                          await invalidateAll();
+                        }}
+                      >
+                        <BookMinus />
+                        Mark as not Read
+                      </DropdownMenu.Item>
+                    {/if}
+                  {/if}
+
+                  {#if data.user}
+                    <DropdownMenu.Item
+                      onSelect={async () => {
+                        const res = await apiClient.updateBookmark({
+                          serieId: data.serie.id,
+                          chapterId: chapter.id,
+                          page: 0,
+                        });
+                        if (!res.success) {
+                          toast.error("Error: " + formatError(res.error));
+                          console.error(formatError(res.error));
+                        }
+
+                        await invalidateAll();
+                      }}
+                    >
+                      <Bookmark />
+                      Set as Bookmark
+                    </DropdownMenu.Item>
+                  {/if}
+                </DropdownMenu.Group>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
+          {/if}
+
+          {#if data.user}
+            <Checkbox
+              checked={isSelected(chapter.id)}
+              onCheckedChange={() => {
+                selectedChapters.push(chapter.id);
+              }}
+            />
+          {/if}
         </div>
       </div>
-    {/snippet}
-
-    {#snippet editChapter(chapter: Chapter, index: number)}
-      <button
-        class="group relative flex items-center gap-2 border-b pb-1 pr-4"
-        onclick={(e) => {
-          if (e.shiftKey) {
-            const firstSelected =
-              selectedChapters.length <= 0 ? 0 : selectedChapters[0];
-            let first = data.chapters.findIndex((i) => i.id === firstSelected);
-
-            let last = index;
-            if (first > last) {
-              const tmp = last;
-              last = first;
-              first = tmp;
-            }
-
-            const items = [];
-            const numItems = last - first + 1;
-            for (let i = 0; i < numItems; i++) {
-              items.push(first + i);
-            }
-
-            const ids = items.map((i) => data.chapters[i].id);
-            selectedChapters = ids;
-          } else {
-            if (isSelected(chapter.id)) {
-              selectedChapters = selectedChapters.filter(
-                (id) => id !== chapter.id,
-              );
-            } else {
-              selectedChapters.push(chapter.id);
-            }
-          }
-        }}
-      >
-        <div class="flex flex-grow items-center gap-2">
-          <img
-            class="h-14 w-10 rounded object-cover"
-            loading="lazy"
-            src={chapter.coverArt.small}
-            alt="Chapter Cover"
-          />
-          <div class="flex flex-col gap-1">
-            <a
-              class="line-clamp-1 font-medium"
-              title={chapter.name}
-              href={`/view/${chapter.id}`}
-            >
-              {chapter.name}
-            </a>
-            <!-- {#if chapter.user?.isMarked}
-                <p class="line-clamp-1 text-start text-sm font-light">Read</p>
-              {/if} -->
-          </div>
-        </div>
-        <div class="flex items-center gap-2">
-          <Checkbox checked={isSelected(chapter.id)} />
-        </div>
-      </button>
-    {/snippet}
-
-    {#each data.chapters as chapter, i}
-      {#if selectedChapters.length > 0}
-        {@render editChapter(chapter, i)}
-      {:else}
-        {@render normalChapter(chapter)}
-      {/if}
+      <Separator />
     {/each}
   </div>
 
   <div class="h-4"></div>
 
-  {#if selectedChapters.length > 0}
+  {#if isEditing}
     <div
-      class="sticky bottom-4 border bg-background px-6 py-3 text-foreground"
+      class="sticky bottom-4 border border-border/40 bg-background bg-background/95 px-6 py-3 text-foreground backdrop-blur supports-[backdrop-filter]:bg-background/60"
     >
       <div class="flex flex-col justify-center gap-2 md:flex-row">
         <div class="flex gap-2">
