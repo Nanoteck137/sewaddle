98b8396f5970219cb91d3936de84d09c6fd6aac3
Patrik M. Rosenström <patrik.millvik@gmail.com>
2025-01-06 04:17:48 +0100

    Added some serie commands to cli

---
+++ cmd/sewaddle-cli/cmd/serie.go
@@ -0,0 +1,91 @@
+package cmd
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"os"
+	"path"
+
+	"github.com/nanoteck137/sewaddle/cmd/sewaddle-cli/api"
+	"github.com/nanoteck137/sewaddle/core/log"
+	"github.com/spf13/cobra"
+)
+
+var serieCmd = &cobra.Command{
+	Use: "serie",
+}
+
+var serieNewCmd = &cobra.Command{
+	Use: "new <NAME>",
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		cover, _ := cmd.Flags().GetString("cover")
+		server, _ := cmd.Flags().GetString("server")
+
+		name := args[0]
+
+		apiClient := api.New(server)
+
+		serie, err := apiClient.CreateSerie(api.CreateSerieBody{
+			Name: name,
+		}, api.Options{})
+		if err != nil {
+			log.Fatal("Failed to create serie", "err", err)
+		}
+
+		fmt.Printf("Serie ID: %s\n", serie.SerieId)
+
+		setSerieCover := func(filename string) error {
+			var b bytes.Buffer
+			w := multipart.NewWriter(&b)
+
+			dw, err := w.CreateFormFile("cover", path.Base(filename))
+			if err != nil {
+				return err
+			}
+
+			src, err := os.Open(filename)
+			if err != nil {
+				return err
+			}
+			defer src.Close()
+
+			_, err = io.Copy(dw, src)
+			if err != nil {
+				return err
+			}
+
+			err = w.Close()
+			if err != nil {
+				return err
+			}
+
+			_, err = apiClient.ChangeSerieCover(serie.SerieId, &b, api.Options{
+				Boundary: w.Boundary(),
+			})
+			if err != nil {
+				return err
+			}
+
+			return nil
+		}
+
+		if cover != "" {
+			fmt.Println("Setting serie cover...")
+			err = setSerieCover(cover)
+			if err != nil {
+				log.Fatal("Failed to set serie cover", "err", err)
+			}
+		}
+	},
+}
+
+func init() {
+	serieNewCmd.Flags().String("cover", "", "Serie cover filename (optional)")
+
+	serieCmd.AddCommand(serieNewCmd)
+
+	rootCmd.AddCommand(serieCmd)
+}
